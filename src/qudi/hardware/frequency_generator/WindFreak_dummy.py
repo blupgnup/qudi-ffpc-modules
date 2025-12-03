8d095dc730f658816b074ee1747c8f12acb5e98c
Pierre <pierre.mahiou@mirega.com>
2025-12-03 14:31:50 +0100

    Updated dummy windfreak class to stick closer to real hardware class
    Easier for debugging

--- src/qudi/hardware/frequency_generator/WindFreak_dummy.py
+++ src/qudi/hardware/frequency_generator/WindFreak_dummy.py
@@ -22,49 +22,82 @@ top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi
 
 from qudi.core.configoption import ConfigOption
 from qudi.interface.frequency_generator_interface import FrequencyGeneratorInterface
+from qudi.util.mutex import Mutex
 
+from types import SimpleNamespace
+
+class DummySynthHD:
+    def __init__(self, **kwargs):
+        # Each channel is just an attribute bag
+        self.channels = [
+            SimpleNamespace(enable = True, power=0, frequency=0, phase=0),
+            SimpleNamespace(enable = False, power=0, frequency=0, phase=0)
+        ]
+
+    def __getitem__(self, index):
+        return self.channels[index]
 
 class WindFreak(FrequencyGeneratorInterface):
     _serial_device = ConfigOption('serial_device', 'COM3', missing='warn')
 
     def __init__(self, config, **kwargs):
         super().__init__(config=config, **kwargs)
-
-
+        self.lock = Mutex()
+        
     def on_activate(self):
-        """ Dummy WindFreak, no activation"""
-        pass
+        """ Dummy WindFreak, no real activation"""
+        self.synth = DummySynthHD()
+        for s in self.synth:
+            s.enable = False
+            s.power = -2
+            s.frequency = 500
+            s.phase = 0
 
     def on_deactivate(self):
+        self.generator_off(ch=0)
+        self.generator_off(ch=1)
         pass
 
     def generator_on(self, ch=None):
+        self.synth[ch].enable = True
         pass
 
     def generator_off(self, ch=None):
+        self.synth[ch].enable = False
         pass
 
     def set_power_level(self, amplitude=None, ch=None):
+        self.synth[ch].power = amplitude
         pass
     
     def get_power_level(self, ch=None):
-        return 0
+        return self.synth[ch].power
 
     def set_frequency(self, freq=None, ch=None):
+        self.synth[ch].frequency = freq
         pass
 
     def get_frequency(self, ch=None):
-        return 666
+        return self.synth[ch].frequency
 
     def set_phase(self, phase=None, ch=None):
+        self.synth[ch].phase = phase
         pass
     
     def get_phase(self, ch=None):
-        return 0
+        return self.synth[ch].phase
 
     def get_temp(self, ch=None):
-        return 1062
+        return 10.62
     
     def get_active_channels(self):
         stat = []
+        if self.synth[0].enable is True:
+            stat.append(1)
+        else:
+            stat.append(0)
+        if self.synth[1].enable is True:
+            stat.append(1)
+        else:
+            stat.append(0)
         return stat
